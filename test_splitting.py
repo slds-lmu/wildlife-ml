990c61753fe4c97f47bdc353db1f14bf22a0796a
lisa-wm <wml1293@outlook.de>
2022-06-13 16:00:04 +0200

    added general csv reader

--- test_splitting.py
+++ test_splitting.py
@@ -4,17 +4,15 @@ from wildlifeml.data import do_train_split
 from wildlifeml.preprocessing.megadetector import MegaDetector
 
 detect = True
-megadetector = MegaDetector(batch_size=1, confidence_threshold=0.8)
+megadetector = MegaDetector(batch_size=512, confidence_threshold=0.8)
 
 root = '../../../../common/bothmannl/'
 img_dir = os.path.join(root, 'wildlife_images/usecase2/original_images/')
-megadetector.predict_directory(
-    img_dir, output_file=os.path.join(root, 'metadata/uc2_md_new.json')
-)
+megadetector.predict_directory(img_dir, output_file='uc2_md_new.json')
 
 train_keys, val_keys, test_keys = do_train_split(
     label_file_path=os.path.join(root, 'metadata/uc2_labels.csv'),
-    detector_file_path=os.path.join(root, 'metadata/uc2_md_new.json'),
+    detector_file_path='uc2_md_new.json',
     min_threshold=0.9,
     splits=(0.6, 0.1, 0.3),
     strategy='random',
