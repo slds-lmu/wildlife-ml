76068f9ffc9f03b94ff8985c631e420ce72c3bcf
lisa-wm <wml1293@outlook.de>
2022-06-14 08:08:46 +0200

    .

--- test_splitting.py
+++ test_splitting.py
@@ -17,13 +17,12 @@ label_dict = [{x['orig_name']: x['true_class'] for x in label_file}]
 save_as_csv(label_dict, 'uc2_labels_new.csv', header=['id', 'class'])
 
 meta_file = label_file
-breakpoint()
-# meta_dict = [
-#     {
-#         x['orig_name']: {'class': x['true_class'], 'stratifier': x['station']}
-#         for x in label_file
-#     }
-# ]
+meta_dict = [
+    {
+        x['orig_name']: {'class': x['true_class'], 'stratifier': x['station']}
+        for x in label_file
+    }
+]
 # save_as_csv(meta_dict, 'uc2_meta.csv', header=['id', 'class', 'stratifier'])
 
 train_keys, val_keys, test_keys = do_train_split(
