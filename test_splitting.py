389bd95ccb89d8b9c23c346fb6452f1daa1d4374
lisa-wm <wml1293@outlook.de>
2022-06-14 07:25:31 +0200

    .

--- test_splitting.py
+++ test_splitting.py
@@ -8,7 +8,8 @@ megadetector = MegaDetector(batch_size=1, confidence_threshold=0.0)
 
 root = '/common/bothmannl/'
 img_dir = os.path.join(root, 'wildlife_images/usecase2/original_images/')
-megadetector.predict_directory(img_dir, output_file='uc2_md_new.json')
+if detect:
+    megadetector.predict_directory(img_dir, output_file='uc2_md_new.json')
 
 train_keys, val_keys, test_keys = do_train_split(
     label_file_path=os.path.join(root, 'metadata/uc2_labels.csv'),
