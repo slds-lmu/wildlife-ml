d5136265f36eeb83f3c4930ee2191eb29caa12ae
lisa-wm <wml1293@outlook.de>
2022-07-07 10:19:45 +0200

    put in requested changes

--- scripts/test_al.sh
+++ scripts/test_al.sh
@@ -1,6 +1,6 @@
 rm -rf example_data/active/*
 rm .activecache.json
-python scripts/test_al.py \
+python test_al.py \
 -di='example_data/images/' \
 -lf='example_data/labels.csv' \
 -df='example_data/_megadetector.json' \
