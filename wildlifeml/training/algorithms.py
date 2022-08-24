821cbecd75ba883e2b769dd037e4e9af1c685a24
Tobias Weber <tobias.weber@stat.uni-muenchen.de>
2022-08-24 17:38:10 +0200

    Integrated tuning procedure into trainer interface

--- wildlifeml/training/algorithms.py
+++ wildlifeml/training/algorithms.py
@@ -1,21 +1,24 @@
 """Classes and functions for creating search and schedule algorithms."""
 
-from typing import (Callable, Final)
+from typing import Callable, Final
+
 from ray.tune.schedulers import ASHAScheduler
 from ray.tune.suggest.hyperopt import HyperOptSearch
 
 AVAILABLE_ALGORITHMS: Final = {
-    'hyperoptsearch': HyperOptSearch, 
-    'ashascheduler': ASHAScheduler, 
-    None: None}
+    'hyperoptsearch': HyperOptSearch,
+    'ashascheduler': ASHAScheduler,
+}
+
 
 class AlgorithmFactory:
     """Factory for creating RayTune algorithm objects."""
-    
+
     @staticmethod
     def get(alg_id: str) -> Callable:
-        """
-        Return an initialized algorithm instance from an identifier.
-        """
+        """Return an algorithm object from an identifier."""
+        return AVAILABLE_ALGORITHMS.get(alg_id)  # type: ignore
 
-        return AVAILABLE_ALGORITHMS[alg_id]
+    def __call__(self, alg_id: str) -> Callable:
+        """Return an algorithm object from an identifier."""
+        return AlgorithmFactory.get(alg_id)
