de4ca553bbf668fb30e5cbf33573cf97f0b78db4
Lisa Wimmer <wml1293@outlook.de>
2022-10-19 16:05:56 +0200

    change weight paths

--- wildlifeml/training/models.py
+++ wildlifeml/training/models.py
@@ -7,30 +7,30 @@ from tensorflow.keras.models import Sequential
 
 AVAILABLE_MODELS: Final = {
     'resnet50': {
-        'model': tf.keras.applications.resnet_v2.ResNet50V2,
+        'model': tf.keras.applications.ResNet50V2,
         'input_shape': (224, 224, 3),
         'preproc_func': tf.keras.applications.resnet_v2.preprocess_input,
     },
-    'inceptionresnetv2': {
-        'model': tf.keras.applications.inception_resnet_v2.InceptionResNetV2,
-        'input_shape': (299, 299, 3),
-        'preproc_func': tf.keras.applications.inception_resnet_v2.preprocess_input,
-    },
-    'vgg19': {
-        'model': tf.keras.applications.vgg19.VGG19,
-        'input_shape': (224, 224, 3),
-        'preproc_func': tf.keras.applications.vgg19.preprocess_input,
-    },
-    'xception': {
-        'model': tf.keras.applications.xception.Xception,
-        'input_shape': (299, 299, 3),
-        'preproc_func': tf.keras.applications.xception.preprocess_input,
-    },
-    'densenet201': {
-        'model': tf.keras.applications.densenet.DenseNet201,
-        'input_shape': (224, 224, 3),
-        'preproc_func': tf.keras.applications.densenet.preprocess_input,
-    },
+    # 'inceptionresnetv2': {
+    #     'model': tf.keras.applications.inception_resnet_v2.InceptionResNetV2,
+    #     'input_shape': (299, 299, 3),
+    #     'preproc_func': tf.keras.applications.inception_resnet_v2.preprocess_input,
+    # },
+    # 'vgg19': {
+    #     'model': tf.keras.applications.vgg19.VGG19,
+    #     'input_shape': (224, 224, 3),
+    #     'preproc_func': tf.keras.applications.vgg19.preprocess_input,
+    # },
+    # 'xception': {
+    #     'model': tf.keras.applications.xception.Xception,
+    #     'input_shape': (299, 299, 3),
+    #     'preproc_func': tf.keras.applications.xception.preprocess_input,
+    # },
+    # 'densenet201': {
+    #     'model': tf.keras.applications.densenet.DenseNet201,
+    #     'input_shape': (224, 224, 3),
+    #     'preproc_func': tf.keras.applications.densenet.preprocess_input,
+    # },
 }
 
 
